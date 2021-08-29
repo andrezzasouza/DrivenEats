153ddb4d25130df67e049b3ae3dbe2338059d082
Andrezza Souza <andrezzasouza@id.uff.br>
2021-08-28 22:00:01 -0300

    Control footer button from parent component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import Header from "./Header";
 import Container from "./Container";
 import FooterMenu from "./FooterMenu";
 
+import React, {useState} from "react";
+
 export default function App () {
+
+  const originalText = "Selecione os 3 itens para fechar o pedido";
+  const readyText = "Fechar pedido"
+
+  const [button, setButton] = useState("order-in-progress");
+  const [text, setText] = useState(originalText)
+
+  function enableButton () {
+    setButton("order-in-progress order-ready");
+    setText(readyText);
+  }
+
   return (
     <>
       <Header />
       <Container />
-      <FooterMenu />
+      <FooterMenu 
+        button={button} 
+        enableButton={enableButton} 
+        text={text} 
+      />
     </>
   );
 }
--- src/FooterMenu.js
+++ src/FooterMenu.js
@@ -1,19 +1,6 @@
-import React, {useState} from "react";
-
 export default function FooterMenu (props) {
 
-  const originalText = "Selecione os 3 itens para fechar o pedido";
-  const readyText = "Fechar pedido"
-
-  const [button, setButton] = useState("order-in-progress");
-  const [text, setText] = useState(originalText)
-
- 
-
-  function enableButton () {
-    setButton("order-in-progress order-ready");
-    setText(readyText);
-  }
+  const {button, enableButton, text} = props;
 
   return (
     <div className="footer-menu">
