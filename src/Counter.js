59e85ee4ad24eac28dfcded1bb4de317b9cd5c09
Andrezza Souza <andrezzasouza@id.uff.br>
2021-08-28 21:34:03 -0300

    Change button style

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,5 @@
-// import React, {useState} from "react";
-
-
 export default function Counter (props) {
 
-    console.log(props)
-
-    // const [counter, setCounter] = useState(1);
     return (
         <div className="counter">
             <button className="minus" onClick={props.decreaseAmount}>-</button>
--- src/FooterMenu.js
+++ src/FooterMenu.js
@@ -1,11 +1,28 @@
-export default function FooterMenu () {
+import React, {useState} from "react";
+
+export default function FooterMenu (props) {
+
+  const originalText = "Selecione os 3 itens para fechar o pedido";
+  const readyText = "Fechar pedido"
+
+  const [button, setButton] = useState("order-in-progress");
+  const [text, setText] = useState(originalText)
+
+ 
+
+  function enableButton () {
+    setButton("order-in-progress order-ready");
+    setText(readyText);
+  }
+
   return (
     <div className="footer-menu">
-    <button className="order-in-progress">
+    <button className={button} onClick={enableButton}>
       <p className="button-text">
-        Selecione os 3 itens para fechar o pedido
+        {text}
       </p>
     </button>
   </div>
   );
 }
+
